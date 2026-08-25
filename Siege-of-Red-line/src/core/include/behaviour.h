b2586b9e08474ae5ab8ecd459e91de544750e213
Rafi-Sojan <stronggamer5645@gmail.com>
2026-08-25 18:32:26 +0530

    file structure organized, created required directories, created a gitignore file

---
+++ Siege-of-Red-line/src/core/include/behaviour.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+
+
---
+++ Siege-of-Red-line/src/game/include/rendering.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <algorithm>
+
